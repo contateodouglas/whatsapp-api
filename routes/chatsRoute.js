080fae24328795315dab092ef98beddc6d246e44
Douglasffernandes2025 <contateodouglas.f30@gmail.com>
2025-06-19 09:25:44 -0300

    update

--- routes/chatsRoute.js
+++ routes/chatsRoute.js
@@ -7,8 +7,15 @@ import sessionValidator from '../middlewares/sessionValidator.js'
 
 const router = Router()
 
+// Único endpoint para todas as mensagens (compatível com o Trait Laravel)
 router.post(
-  '/send',
+  [
+    '/send',
+    '/user/sent-whatsapp-custom-text/plain-text',
+    '/user/sent-whatsapp-custom-text/text-with-button',
+    '/user/sent-whatsapp-custom-text/text-with-media',
+    '/user/sent-whatsapp-custom-text/text-with-list'
+  ],
   query('id').notEmpty(),
   body('receiver').notEmpty(),
   body('message').notEmpty(),
